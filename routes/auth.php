68bdf2d077e45cdbd7fd8efe9f4bba389a069c01
YusronFauzanN <Yusronsronz7@gmail.com>
2022-05-12 18:50:40 +0700

    oauth

--- routes/auth.php
+++ routes/auth.php
@@ -16,8 +16,13 @@ Route::middleware('guest')->group(function () {
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+    Route::get('login', function(){
+        return view('login');
+    })->middleware('guest')->name('login');
+
+    Route::get('login/admin', [AuthenticatedSessionController::class, 'create'])
+                ->middleware('guest')
+                ->name('login.admin');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
